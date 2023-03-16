16229723bee73ebce6648031adb23ee9e758df9e
tonybruce <iyajianthony59@gmail.com>
2023-03-16 10:52:23 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
 int main(void)
 {
 	/* printing with puts() */
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
