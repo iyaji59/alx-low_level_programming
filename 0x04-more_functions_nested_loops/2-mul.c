8e2c6dbf47afbf75ea1cd47c8eb201e8f4062387
tonybruce <iyajianthony59@gmail.com>
2023-03-23 08:38:46 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - to multiply two parameters
+ * @a: one of the parameters
+ * @b: one of the parameters
+ * Return: Always
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
