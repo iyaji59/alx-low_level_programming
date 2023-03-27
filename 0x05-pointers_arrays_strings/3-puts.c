1995734714eb0a6cf5d8e53307cddaeab54fdec7
tonybruce <iyajianthony59@gmail.com>
2023-03-27 11:21:40 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - puts a string
+ *
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
