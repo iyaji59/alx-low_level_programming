b8ac5330a9ea45010efaf21e5678d1153bab2471
tonybruce <iyajianthony59@gmail.com>
2023-03-30 05:46:40 -0700

    betty corrected task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,14 @@
  */
 char *string_toupper(char *s)
 {
-	int i;
+	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = s[i] - 32;
-		}
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
 	}
+
+	return (s);
 }
