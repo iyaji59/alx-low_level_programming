735cb988e623503faac20cb16133a7e0cafccae6
tonybruce <iyajianthony59@gmail.com>
2023-04-04 06:02:28 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: memory area we are copying to
+ * @src: memory area we are copying from
+ * @n: size of times to be copied
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+}
